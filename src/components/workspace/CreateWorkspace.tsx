74feb19726953c54fb0e718a17fc1b3f394407a2
Dallas Klein <dallasjklein@gmail.com>
2025-01-07 14:40:21 -0500

    styling stuff

--- src/components/workspace/CreateWorkspace.tsx
+++ src/components/workspace/CreateWorkspace.tsx
@@ -8,9 +8,9 @@ interface CreateWorkspaceProps {
 export const CreateWorkspace: React.FC<CreateWorkspaceProps> = ({ onClick }) => (
   <Button 
     onClick={onClick}
-    className="rounded-full bg-charcoal-gray hover:bg-gray-100 hover:text-black outline outline-1 outline-gray-400 dark:hover:bg-gray-800"
+    className="rounded-full bg-charcoal-gray hover:bg-gray-100 hover:text-black outline outline-1 outline-gray-400 dark:hover:bg-gray-800 mt-2"
   >
-    <Plus className="h-5 w-5 inline hover:text-black" />
-    <span className="text-white text-sm ml-1 inline hover:text-black">Add Workspace</span>
+    <Plus className="text-gray-400 h-5 w-5 inline hover:text-black" />
+    <span className="text-gray-400 text-sm ml-1 inline hover:text-black">Add Workspace</span>
   </Button>
 );
