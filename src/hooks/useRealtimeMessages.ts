cb68f3070686184fd999a3b07462292bad9da546
Dallas Klein <dallasjklein@gmail.com>
2025-01-08 21:01:32 -0500

    commenting out reactions for now

--- src/hooks/useRealtimeMessages.ts
+++ src/hooks/useRealtimeMessages.ts
@@ -25,11 +25,11 @@ export const useRealtimeMessages = (channelId: string | undefined) => {
       });
 
       // Subscribe to reactions
-      realtimeService.subscribeToReactions(channelId, ({ eventType, reaction }) => {
+      /*realtimeService.subscribeToReactions(channelId, ({ eventType, reaction }) => {
         if (reaction && reaction.message_id) {
           updateReactions(reaction.message_id);
         }
-      });
+      });*/
     } catch (error) {
       console.error('Error setting up realtime subscriptions:', error);
     }
